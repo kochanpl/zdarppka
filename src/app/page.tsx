b2145904b0a75ff533964100544202df246d2201
raf <twoj.kochan@przyklad.com>
2025-03-17 22:53:34 +0100

    1st2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ScratchCard from "@/components/ScratchCard";
 import WinAlert from "@/components/WinAlert";
 import UserForm from "@/components/UserForm";
